da0f37ba2124845752c51342fbc0a3e6ea329bd2
Wael Muawad <wael.muawad@gmail.com>
2024-02-22 09:42:39 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Google.OrTools.LinearSolver;
+
+Console.WriteLine("Hello, World!");
+
+
+// Create the linear solver with the GLOP backend.
+using Solver solver = Solver.CreateSolver("GLOP");
+if (solver is null)
+{
+    return;
+}
+
+// Create the variables x and y.
+using Variable x = solver.MakeNumVar(0.0, 1.0, "x");
+using Variable y = solver.MakeNumVar(0.0, 2.0, "y");
+
+Console.WriteLine("Number of variables = " + solver.NumVariables());
